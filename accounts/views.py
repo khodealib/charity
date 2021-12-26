f39b2949c047f791e6798eb6606db37f49cc4d1a
Ali Bagheri <khodealib@gmail.com>
2021-12-26 08:44:49 +0330

    implement register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class LogoutAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(
+            data={'message': f'Bye {request.user.username}!'},
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+
+class UserRegistration(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        user_serializer = self.get_serializer(data=request.data)
+        if user_serializer.is_valid():
+            username = user_serializer.validated_data['username']
+            user_serializer.save()
+            return Response(
+                data={'message': '{} is created'.format(username)}
+            )
